efb2813c100d6d5ea97c6a738178c81a8b8fcd32
Priyusha Chinta <priyusha.ch99@gmail.com>
2026-08-18 13:08:53 -0500

    Updated Portfolio

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -12,7 +12,6 @@ type ProjectProps = {
   tech: string[];
   metrics: string[];
   github: string;
-  demo: string;
 };
 
 
@@ -23,7 +22,6 @@ export default function ProjectCard({
   tech,
   metrics,
   github,
-  demo,
 }: ProjectProps) {
 
 
